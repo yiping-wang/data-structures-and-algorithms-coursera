0c95b56735cf541aabda7fff7c43bc67966bd252
Yiping Wang <yiping@wang.vision>
2019-08-25 15:21:16 -0700

    .

--- data-structures/week1_basic_data_structures/2_tree_height/tree_height.py
+++ data-structures/week1_basic_data_structures/2_tree_height/tree_height.py
@@ -2,30 +2,74 @@
 
 import sys
 import threading
+import queue
 
 
-def compute_height(n, parents):
-    # Replace this code with a faster implementation
-    max_height = 0
-    for vertex in range(n):
-        height = 0
-        current = vertex
-        while current != -1:
+class Node:
+    def __init__(self, id):
+        self.children = []
+        self.id = id
+
+    def add_child(self, child):
+        self.children.append(child)
+
+    def get_children(self):
+        return self.children
+
+    def __str__(self):
+        return str(self.id)
+
+    def __repr__(self):
+        children = 'node ' + str(self.id) + ' has children:'
+        for child in self.children:
+            children += ' ' + str(child)
+        return children
+
+
+def build_trees(n_nodes, parents):
+    nodes = []
+    for id in range(n_nodes):
+        node = Node(id)
+        nodes.append(node)
+
+    for child_idx in range(len(parents)):
+        parent_idx = parents[child_idx]
+        if parent_idx == -1:
+            root = child_idx
+        else:
+            nodes[parent_idx].add_child(nodes[child_idx])
+
+    return root, nodes
+
+
+def compute_height(root, tree):
+    height = 0
+    q = queue.Queue(maxsize=len(tree))
+    q.put(tree[root])
+    # use None as a marker to indicate the current level
+    # has been traversed
+    q.put(None)
+
+    while not q.empty():
+        cur_node = q.get()
+        if cur_node is None:
+            if not q.empty():
+                q.put(None)
             height += 1
-            current = parents[current]
-        max_height = max(max_height, height)
-    return max_height
+        else:
+            children = cur_node.get_children()
+            for child in children:
+                q.put(child)
+
+    return height
 
 
 def main():
     n = int(input())
     parents = list(map(int, input().split()))
-    print(compute_height(n, parents))
+    root, tree = build_trees(n, parents)
+    print(compute_height(root, tree))
 
 
-# In Python, the default limit on recursion depth is rather low,
-# so raise it here for this problem. Note that to take advantage
-# of bigger stack, we have to launch the computation in a new thread.
-sys.setrecursionlimit(10**7)  # max depth of recursion
-threading.stack_size(2**27)   # new thread will get stack of such size
-threading.Thread(target=main).start()
+if __name__ == "__main__":
+    main()
