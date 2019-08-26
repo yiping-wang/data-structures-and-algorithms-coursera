07e11e4a1094bdbaf4b0deebb29aa871d9b36826
Yiping Wang <yiping@wang.vision>
2019-08-25 22:37:45 -0700

    .

--- data-structures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
+++ data-structures/week1_basic_data_structures/5_max_sliding_window/max_sliding_window.py
@@ -1,12 +1,33 @@
 # python3
 
+from collections import deque
+
 
 def max_sliding_window_naive(sequence, m):
-    maximums = []
-    for i in range(len(sequence) - m + 1):
-        maximums.append(max(sequence[i:i + m]))
+    q = deque()
+    res = []
+    cur_max = float('-inf')
 
-    return maximums
+    for idx, item in enumerate(sequence):
+        if idx == 0:
+            q.append(item)
+        elif idx == m - 1:
+            q.append(cur_max)
+        elif idx < m:
+            cur_max = item if cur_max < item else cur_max
+        else:
+            drop_item = q[0]
+            psudo_max = q[-1]
+            res.append(max(drop_item, psudo_max))
+            q.popleft()
+            q.append(item)
+
+    drop_item = q[0]
+    psudo_max = q[-1]
+    res.append(max(drop_item, psudo_max))
+
+    return res
+
 
 if __name__ == '__main__':
     n = int(input())
@@ -15,4 +36,3 @@ if __name__ == '__main__':
     window_size = int(input())
 
     print(*max_sliding_window_naive(input_sequence, window_size))
-
