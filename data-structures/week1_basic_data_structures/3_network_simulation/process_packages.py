8d958b0c5fa9e637b3e3d87d531fcbb5cc246eba
Yiping Wang <yiping@wang.vision>
2019-08-25 20:59:02 -0700

    .

--- data-structures/week1_basic_data_structures/3_network_simulation/process_packages.py
+++ data-structures/week1_basic_data_structures/3_network_simulation/process_packages.py
@@ -11,9 +11,30 @@ class Buffer:
         self.size = size
         self.finish_time = []
 
+    def is_full(self):
+        return len(self.finish_time) == self.size
+
     def process(self, request):
-        # write your code here
-        return Response(False, -1)
+        if not self.finish_time:
+            # finish_time is empty
+            cur_finish_time = request.arrived_at + request.time_to_process
+            self.finish_time.append(cur_finish_time)
+            return Response(False, request.arrived_at)
+        else:
+            last_packet_finish_time = self.finish_time[-1]
+
+            # pop from the front of finish_time all the packets which are already
+            # processed by the time new packet arrives.
+            while self.finish_time and self.finish_time[0] <= request.arrived_at:
+                del self.finish_time[0]
+
+            if self.is_full():
+                return Response(True, -1)
+
+            cur_finish_time = last_packet_finish_time + request.time_to_process
+            self.finish_time.append(cur_finish_time)
+
+            return Response(False, last_packet_finish_time)
 
 
 def process_requests(requests, buffer):
