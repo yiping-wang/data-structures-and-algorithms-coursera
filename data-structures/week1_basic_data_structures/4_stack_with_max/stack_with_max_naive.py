a303d8430b5aa4c2d08e388d38ea52a5aba151d4
Yiping Wang <yiping@wang.vision>
2019-08-25 21:48:32 -0700

    .

--- data-structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
+++ data-structures/week1_basic_data_structures/4_stack_with_max/stack_with_max_naive.py
@@ -1,20 +1,51 @@
-#python3
+# python3
+
 import sys
 
-class StackWithMax():
+
+class Stack:
     def __init__(self):
         self.__stack = []
 
-    def Push(self, a):
+    def push(self, a):
         self.__stack.append(a)
 
-    def Pop(self):
+    def pop(self):
         assert(len(self.__stack))
         self.__stack.pop()
 
+    def empty(self):
+        return len(self.__stack) == 0
+
+    def peek(self):
+        assert(len(self.__stack))
+        return self.__stack[-1]
+
+
+class StackWithMax:
+    def __init__(self):
+        self.__stack = []
+        self.max_stack = Stack()
+
+    def Push(self, a):
+        self.__stack.append(a)
+        if self.max_stack.empty():
+            self.max_stack.push(a)
+        else:
+            cur_max = self.max_stack.peek()
+            if cur_max is not None and cur_max <= a:
+                self.max_stack.push(a)
+
+    def Pop(self):
+        assert(len(self.__stack))
+        cur_top = self.__stack.pop()
+        cur_max = self.max_stack.peek()
+        if cur_max == cur_top:
+            self.max_stack.pop()
+
     def Max(self):
         assert(len(self.__stack))
-        return max(self.__stack)
+        return self.max_stack.peek()
 
 
 if __name__ == '__main__':
